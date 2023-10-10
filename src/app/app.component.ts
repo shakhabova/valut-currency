da6d91dc262a94b49f5831233969bc13a92b4a71
Алина Шахабова <alina.shakhabova@yandex.ru>
2023-10-10 13:45:52 +0300

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'valut-currency';
+
+export class AppComponent implements AfterViewInit {
+  @ViewChild('helloModal') helloEl?: ElementRef;
+  modal?: bootstrap.Modal;
+  ngAfterViewInit() {
+    this.modal = new bootstrap.Modal(this.helloEl?.nativeElement, {});
+  }
+ 
+  trigger() {
+    this.modal?.toggle();
+  }
 }
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class HomePageComponent {
+
+}
