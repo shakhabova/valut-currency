215e971cf3c8dd38f2ef87d3c497287f9eaa1d75
Алина Шахабова <alina.shakhabova@yandex.ru>
2023-10-26 17:34:08 +0300

    navigate method for mobile version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { WindowScrolledService } from './window-scrolled.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,10 +13,15 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   public windowScrolled$: Observable<boolean> = this.windowScrolledService.windowScrolled$;
 
-  constructor(private windowScrolledService: WindowScrolledService) {}
+  constructor(private windowScrolledService: WindowScrolledService, private router: Router) {}
 
   public toggleExpander(ev: MouseEvent): void {
     ev.preventDefault();
     document.body.classList.toggle('nav-expanded');
+  }
+
+  public navigate(ev: MouseEvent, url: string): void {
+    this.toggleExpander(ev);
+    this.router.navigateByUrl(url);
   }
 }
